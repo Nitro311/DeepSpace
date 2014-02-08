535badad8d94bea4afdb1e495d51a4f22a440b70
Alex Dutton <alex.dutton@duttonsoftware.com>
2014-02-08 15:24:13 -0800

    added: players, ports, and max weight

--- main.py
+++ main.py
@@ -10,9 +10,47 @@ class Sector:
     def __str__(self):
         return "<sector routes=" + str(self.routes) + " warps=" + str(self.warps) + " name="+self.name +">"
 
+class Player:
+    name=""
+    gold_coins=0
+    
+class Port:
+    type=""
+    resources = {}
+    buy_prices = {}
+    sell_prices = {}
+    strength= 0
+    created_resources = {}
+
+class MiningPort(Port):
+    type = "mining"
+    resources = { "ore": 10000, "iron": 500, "gold": 100, "diamond": 25 }
+    buy_prices = { "food": 20, "tools": 100 }
+    sell_prices = { "ore": 10, "iron": 18, "gold": 100, "diamond": 300 }
+    strength = 10
+    created_resources = { "ore": 100, "iron": 10, "gold": 5, "diamond": 1 }
+
+class ManufacturingPort(Port):
+    type = "manufacturing"
+    resources = { "tools": 10000, "engines": 200 }
+    buy_prices = { "food": 30, "iron": 100 "gold":500 "diamond":400 }
+    sell_prices = { "tools": 10, "engines": 18 }
+    strength = 10
+    created_resources = { "tools": 100, "engines": 10  }
+    
+class FarmingPort(Port):
+    type = "manufacturing"
+    resources = { "wheat": 10000, "food": 200 }
+    buy_prices = {"tools": 100 "engines":500 }
+    sell_prices = { "wheat": 10, "food": 18 }
+    strength = 10
+    created_resources = { "wheat": 100, "food": 10  }
+    
 class Ship:
     moves= 0
     warp_drive=False
+    max_weight=100
+    min_weight = 0
 
 class Junk(Ship):
     moves = 3
@@ -104,5 +142,6 @@ while (having_fun):
     
     #if ship.moves==0:
        #easygui.msgbox("You Have Run Out Of Moves.")
+
    
 raw_input()    
